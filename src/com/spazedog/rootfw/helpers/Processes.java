170dcf4b50085b731dd23a022f3aa5cd4eadef10
Daniel Bergløv <webgeek.eu@gmail.com>
2013-04-01 12:12:28 +0200

    Processes: Add options to reboot and shutdown the device This adds the fallowing options
     * Rebooting the device
     * Rebooting the device into recovery
     * Shut down the device
     * Kill all processes and produce a soft-reboot

--- src/com/spazedog/rootfw/helpers/Processes.java
+++ src/com/spazedog/rootfw/helpers/Processes.java
@@ -80,4 +80,72 @@ public final class Processes {
 		
 		return status;
 	}
+	
+	/*
+	 * This function will send the SIGTERM signal to all processes except init.
+	 * This will produce a soft-reboot which will reboot all Android's core and main services, 
+	 * but without rebooting the actual device. 
+	 */
+	public Boolean killAll() {
+		RootFW.log(TAG + ".killAll", "Sending SIGTERM signal to all processes");
+		
+		ShellResult result = ROOTFW.runShell(ShellCommand.makeCompatibles("%binary echo 1 > /proc/sys/kernel/sysrq && %binary echo s > /proc/sysrq-trigger && %binary echo e > /proc/sysrq-trigger"));
+		Boolean status = result.getResultCode() == 0 ? true : false;
+		
+		if (!status) {
+			RootFW.log(TAG + ".killAll", "It was not possible to send SIGTERM to all processes", RootFW.LOG_WARNING);
+		}
+		
+		return status;
+	}
+	
+	/*
+	 * This function will reboot the device. It will use the toolbox reboot command when possible,
+	 * or trigger sysrg if toolbox does not support reboot. We cannot use busybox as it's reboot command does not work.
+	 */
+	public Boolean reboot() {
+		RootFW.log(TAG + ".reboot", "Preparing to reboot the device");
+		
+		ShellResult result = ROOTFW.runShell("toolbox reboot");
+		
+		if (result.getResultCode() > 0) {
+			RootFW.log(TAG + ".reboot", "The device does not support the reboot command. Triggering sysrq instead");
+			result = ROOTFW.runShell(ShellCommand.makeCompatibles("%binary echo 1 > /proc/sys/kernel/sysrq && %binary echo s > /proc/sysrq-trigger && %binary echo b > /proc/sysrq-trigger"));
+		}
+		
+		return result.getResultCode() == 0;
+	}
+	
+	/*
+	 * This function will reboot the device into recovery using the toolbox reboot command.
+	 * The function still needs an sysrg trigger as failsafe
+	 */
+	public Boolean rebootRecovery() {
+		RootFW.log(TAG + ".rebootRecovery", "Preparing to reboot the device into recovery");
+		
+		ShellResult result = ROOTFW.runShell("toolbox reboot recovery");
+		
+		if (result.getResultCode() > 0) {
+			/* TODO: Add a sysrq trigger for this event */
+			RootFW.log(TAG + ".rebootRecovery", "The device does not support the reboot command", RootFW.LOG_WARNING);
+		}
+		
+		return result.getResultCode() == 0;
+	}
+	
+	/*
+	 * This function will trigger sysrq into shutting down the device. 
+	 * Note that this will not proper unmount the filesystems or send any SIGTERM to any processes
+	 */
+	public Boolean shutdown() {
+		RootFW.log(TAG + ".shutdown", "Preparing to shut down the device");
+		
+		ShellResult result = ROOTFW.runShell(ShellCommand.makeCompatibles("%binary echo 1 > /proc/sys/kernel/sysrq && %binary echo s > /proc/sysrq-trigger && %binary echo o > /proc/sysrq-trigger"));
+		
+		if (result.getResultCode() > 0) {
+			RootFW.log(TAG + ".shutdown", "It was not possible to shut down the device", RootFW.LOG_WARNING);
+		}
+		
+		return result.getResultCode() == 0;
+	}
 }
