f5905d8f71e4fff47e42be5e619063743883a0d0
Daniel Bergløv <d.bergloev@gmail.com>
2013-08-18 10:20:42 +0200

    FileExtender: Add listing tools

--- src/com/spazedog/lib/rootfw3/extenders/FileExtender.java
+++ src/com/spazedog/lib/rootfw3/extenders/FileExtender.java
@@ -26,6 +26,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import com.spazedog.lib.rootfw3.containers.Data;
 import com.spazedog.lib.rootfw3.extenders.ShellExtender.ShellResult;
@@ -52,6 +53,9 @@ import com.spazedog.lib.rootfw3.interfaces.ExtenderGroup;
  * </dl>
  */
 public class FileExtender implements ExtenderGroup {
+	
+	private final static Pattern oPatternStatSplitter = Pattern.compile("\\|");
+	private final static Pattern oPatternStatSearch = Pattern.compile("^([a-z-]+)(?:[ \t]+([0-9]+))?[ \t]+([0-9a-z_]+)[ \t]+([0-9a-z_]+)(?:[ \t]+(?:([0-9]+),[ \t]+)?([0-9]+))?[ \t]+([A-Za-z]+[ \t]+[0-9]+[ \t]+[0-9:]+|[0-9-/]+[ \t]+[0-9:]+)[ \t]+(?:(.*) -> )?(.*)$");
 	
 	protected ShellExtender mShell;
 	
@@ -393,7 +397,7 @@ public class FileExtender implements ExtenderGroup {
 	 *     A new instance of this class representing another relative file or folder
 	 */
 	public FileExtender open(String file) {
-		if (!isDirectory()) {
+		if (isDirectory()) {
 			String path = mFile.getAbsolutePath().endsWith("/") ? mFile.getAbsolutePath().substring(0, mFile.getAbsolutePath().length()-1) : mFile.getAbsolutePath();
 			String fileName = file.startsWith("/") ? file.substring(0) : file;
 			
@@ -414,11 +418,287 @@ public class FileExtender implements ExtenderGroup {
 	}
 	
 	/**
+	 * Get information about this file or folder. This will return information like 
+	 * size (on files), path to linked file (on links), permissions, group, user etc.
+	 * 
+	 * @return
+	 *     A new {@link FileStat} object with all the file information
+	 */
+	public FileStat getDetails() {
+		if (exists()) {
+			FileStat[] stat = getDetailedList(1);
+			
+			if (stat != null && stat.length > 0) {
+				String name = mFile.getName();
+				
+				if (stat[0].name().equals(".")) {
+					stat[0].mName = name;
+					
+					return stat[0];
+					
+				} else if (stat[0].name().equals(name)) {
+					return stat[0];
+					
+				} else {
+					/* On devices without busybox, we could end up using limited toolbox versions
+					 * that does not support the "-a" argument in it's "ls" command. In this case,
+					 * we need to do a more manual search for folders.
+					 */
+					stat = open("../").getDetailedList();
+					
+					if (stat != null && stat.length > 0) {
+						for (int i=0; i < stat.length; i++) {
+							if (stat[i].name().equals(name)) {
+								return stat[i];
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * @see #getDetailedList(Integer)
+	 */
+	public FileStat[] getDetailedList() {
+		return getDetailedList(0);
+	}
+	
+	/**
+	 * This is the same as {@link #getDetails()}, only this will provide a whole list 
+	 * with information about each item in a directory.
+	 * 
+	 * @param maxLines
+	 *     The max amount of lines to return. This also excepts negative numbers. 
+	 * 
+	 * @return
+	 *     An array of {@link FileStat} object
+	 */
+	public FileStat[] getDetailedList(Integer maxLines) {
+		if (exists()) {
+			ShellResult result = mShell.buildAttempts("%binary ls -lna '" + mFile + "'", "%binary ls -la '" + mFile + "'", "%binary ls -ln '" + mFile + "'", "%binary ls -l '" + mFile + "'").run();
+			
+			if (result.wasSuccessful()) {
+				List<FileStat> list = new ArrayList<FileStat>();
+				String[] lines = result.trim().getArray();
+				Integer maxIndex = (maxLines == null || maxLines == 0 ? lines.length : (maxLines < 0 ? lines.length + maxLines : maxLines));
+				
+				for (int i=0,indexCount=1; i < lines.length && indexCount <= maxIndex; i++) {
+					/* There are a lot of different output from the ls command, depending on the arguments supported, whether we used busybox or toolbox and the versions of the binaries. 
+					 * We need some serious regexp help to sort through all of the different output options. 
+					 */
+					String[] parts = oPatternStatSplitter.split( oPatternStatSearch.matcher(lines[i]).replaceAll("$1|$3|$4|$5|$6|$8|$9") );
+					
+					if (parts.length == 7) {
+						String partType = parts[0].substring(0, 1).equals("-") ? "f" : parts[0].substring(0, 1);
+						String partAccess = parts[0];
+						Integer partUser = Integer.parseInt(parts[1]);
+						Integer partGroup = Integer.parseInt(parts[2]);
+						Long partSize = parts[4].equals("null") || !parts[3].equals("null") ? 0L : Long.parseLong(parts[4]);
+						String partMM = parts[3].equals("null") ? null : parts[3] + ":" + parts[4];
+						String partName = parts[5].equals("null") ? parts[6].substring( parts[6].lastIndexOf("/") + 1 ) : parts[5].substring( parts[5].lastIndexOf("/") + 1 );
+						String partLink = parts[5].equals("null") ? null : parts[6];
+						String partPermission = "";
+						String[] partModAssambler = new String[] {partAccess.substring(1), partAccess.substring(1, 4), partAccess.substring(4, 7), partAccess.substring(7, 10)};
+						
+						for (int x=0; x < partModAssambler.length; x++) {
+							Integer mod = (x == 0 && partModAssambler[x].charAt(2) == 's') || (x > 0 && partModAssambler[x].charAt(0) == 'r') ? 4 : 0;
+							mod += (x == 0 && partModAssambler[x].charAt(5) == 's') || (x > 0 && partModAssambler[x].charAt(1) == 'w') ? 2 : 0;
+							mod += (x == 0 && partModAssambler[x].charAt(8) == 't') || (x > 0 && partModAssambler[x].charAt(2) == 'x') ? 1 : 0;
+							
+							partPermission += "" + mod;
+						}
+						
+						if (partName.contains("/")) {
+							partName = partName.substring( partName.lastIndexOf("/")+1 );
+						}
+						
+						FileStat stat = new FileStat();
+						stat.mAccess = partAccess;
+						stat.mPermission = partPermission;
+						stat.mUser = partUser;
+						stat.mGroup = partGroup;
+						stat.mLink = partLink;
+						stat.mMM = partMM;
+						stat.mName = partName;
+						stat.mSize = partSize;
+						stat.mType = partType;
+						
+						list.add(stat);
+						
+						indexCount++;
+					}
+				}
+				
+				return list.toArray( new FileStat[ list.size() ] );
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * This will provide a simple listing of a directory.
+	 * For a more detailed listing, use {@link #getDetailedList()} instead. 
+	 * 
+	 * @return
+	 *     An array with the names of all the items in the directory
+	 */
+	public String[] getList() {
+		if (isDirectory()) {
+			if (!iRestricted && mFile.canRead()) {
+				return mFile.list();
+				
+			} else {
+				ShellResult result = mShell.buildAttempts("%binary ls -1a '" + mFile.getAbsolutePath() + "'", "%binary ls -1 '" + mFile.getAbsolutePath() + "'").run();
+				
+				if (result.wasSuccessful()) {
+					return result.trim().getArray();
+					
+				} else {
+					/* Most toolbox versions does not support the "-1" argument in the "ls" command.
+					 * In these cases without busybox, we fall back to using the statList() method
+					 * and manually create our list.
+					 */
+					FileStat[] stat = getDetailedList();
+					
+					if (stat != null) {
+						String[] output = new String[ stat.length ];
+						
+						for (int i=0; i < output.length; i++) {
+							output[i] = stat[i].name();
+						}
+						
+						return output;
+					}
+				}
+			}
+			
+		} else if (exists()) {
+			return new String[]{ mFile.getName() };
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * This is the same as {@link #getList()}, only this will provide an array of 
+	 * {@link FileExtender} objects for each item in the directory.
+	 * 
+	 * @return
+	 *     An array of {@link FileExtender} objects
+	 */
+	public FileExtender[] getObjectList() {
+		if (exists()) {
+			String[] list = getList();
+			FileExtender[] output = new FileExtender[ list.length ];
+			
+			for (int i=0; i < list.length; i++) {
+				output[i] = open( list[i] );
+			}
+			
+			return output;
+		}
+		
+		return null;
+	}
+	
+	/**
 	 * This class is extended from the Data class. As for now, there is nothing custom added to this class. But it might differ from the Data class at some point.
 	 */
 	public static class FileData extends Data<FileData> {
 		public FileData(String[] lines) {
 			super(lines);
+		}
+	}
+	
+	/**
+	 * This class is a container which is used by {@link FileExtender#getDetails()} and {@link FileExtender#getDetailedList(Integer)}
+	 */
+	public static class FileStat {
+		private String mName;
+		private String mLink;
+		private String mType;
+		private Integer mUser;
+		private Integer mGroup;
+		private String mAccess;
+		private String mPermission;
+		private String mMM;
+		private Long mSize;
+		
+		/** 
+		 * @return
+		 *     The filename
+		 */
+		public String name() {
+			return mName;
+		}
+		
+		/** 
+		 * @return
+		 *     The path to the original file if this is a symbolic link
+		 */
+		public String link() {
+			return mLink;
+		}
+		
+		/** 
+		 * @return
+		 *     The file type ('d'=>Directory, 'f'=>File, 'b'=>Block Device, 'c'=>Character Device, 'l'=>Symbolic Link)
+		 */
+		public String type() {
+			return mType;
+		}
+		
+		/** 
+		 * @return
+		 *     The owners user id
+		 */
+		public Integer user() {
+			return mUser;
+		}
+		
+		/** 
+		 * @return
+		 *     The owners group id
+		 */
+		public Integer group() {
+			return mGroup;
+		}
+		
+		/** 
+		 * @return
+		 *     The files access string like (drwxrwxr-x)
+		 */
+		public String access() {
+			return mAccess;
+		}
+		
+		/** 
+		 * @return
+		 *     The file permissions like (0755)
+		 */
+		public String permission() {
+			return mPermission;
+		}
+		
+		/** 
+		 * @return
+		 *     The file Major:Minor number (If this is a Block or Character device file)
+		 */
+		public String mm() {
+			return mMM;
+		}
+		
+		/** 
+		 * @return
+		 *     The file size in bytes
+		 */
+		public Long size() {
+			return mSize;
 		}
 	}
 }
